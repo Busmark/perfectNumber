431452d9e3440d9afbf03c05300b97a0e0f97341
Busmark <oscar.husmark@redmind.se>
2019-08-09 15:58:05 +0200

    first commit

--- src/main/java/se/oshu/perfectnumber/PerfectnumberApplication.java
+++ src/main/java/se/oshu/perfectnumber/PerfectnumberApplication.java
@@ -2,19 +2,11 @@ package se.oshu.perfectnumber;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class PerfectnumberApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PerfectnumberApplication.class, args);
-	}
-
-	@GetMapping("/is_perfect_number")
-	public String hello() {
-		return "hello Oscar!";
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PerfectnumberApplication.class, args);
+    }
 }
